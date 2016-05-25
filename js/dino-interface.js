dd6b82372b38c1e65a8dc17c3502a5fb29306e46
Ashley Maceli and Ryan Carlile <ashley@>
2016-05-25 14:20:13 -0700

    Add an emoji json file

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -4,13 +4,23 @@ $(document).ready(function() {
 
   $('.button').click(function() {
 
-    $.get('http://corpora-api.herokuapp.com/corporations/fortune500').then(function(response) {
-      $('#tweet').html(response.data.companies[Math.floor((Math.random() * 499) + 1)]);
+    $.get('/emoji.json').then(function(response) {
+      $('#emoji').html(response.seaEmoji);
       console.log(response);
     });
 
+    $.get('http://corpora-api.herokuapp.com/corporations/fortune500').then(function(response) {
+      $('#companies').html(response.data.companies[Math.floor((Math.random() * 499) + 1)]);
+
+    });
+
+    $.get('http://corpora-api.herokuapp.com/foods/fruits').then(function(response) {
+      $('#fruits').html(response.data.fruits[Math.floor((Math.random() * 79) + 1)]);
+    
+    });
+
       $.get('http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=1&words=1').then(function(response){
-        $('#some-awesome-container').html(response);
+        $('#dinos').html(response);
 
     }, function() {
       console.log('Where did all the dinosaurs go?');
