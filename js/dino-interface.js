ac1e7a3d7365bd21c7463bb96ca74d3f81a387e3
Ashley Maceli and Ryan Carlile <ashley@>
2016-05-25 16:38:52 -0700

    Add a sentence with our generative objects

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,12 +1,18 @@
 // var apiKey = "YOUR-API-KEY-GOES-HERE";
+// var corpora = require('corpora-project');
+// var corporaProject = require('corpora-project');
 
 $(document).ready(function() {
+  // console.log(corpora.getCategories());
 
-  $('.button').click(function() {
+  $('.btn').click(function() {
+    // $('#emoji').text(corpora.getFile('emojis', 'sea_emoji'));
+    $.get('/sentence.json').then(function(response) {
+      $('#verb').html(response.sentenceStructure[Math.floor((Math.random() * 7) + 1)]);
+    });
 
     $.get('/emoji.json').then(function(response) {
-      $('#emoji').html(response.seaEmoji);
-      console.log(response);
+      $('#emoji').html(response.seaEmoji[Math.floor((Math.random() * 16) + 1)]);
     });
 
     $.get('http://corpora-api.herokuapp.com/corporations/fortune500').then(function(response) {
@@ -16,7 +22,7 @@ $(document).ready(function() {
 
     $.get('http://corpora-api.herokuapp.com/foods/fruits').then(function(response) {
       $('#fruits').html(response.data.fruits[Math.floor((Math.random() * 79) + 1)]);
-    
+
     });
 
       $.get('http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=1&words=1').then(function(response){
