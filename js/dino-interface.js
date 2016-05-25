ddb9848c7b7a726059469b40464b8f1b77b72fd9
Ashley Maceli and Ryan Carlile <ashley@>
2016-05-25 10:46:15 -0700

    Focus ipsum on one word and paragraph

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -2,8 +2,12 @@
 
 $(document).ready(function() {
   $('.button').click(function() {
-    $.get('http://dinoipsum.herokuapp.com/api/?format=html').then(function(response) {
-      $('#some-awesome-container').html(response);
+    // $.get('http://dinoipsum.herokuapp.com/api/?format=html').then(function(response) {
+    //   $('#some-awesome-container').html(response);
+
+      $.get('http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=1&words=1').then(function(response){
+        $('#some-awesome-container').html(response);
+
     }, function() {
       console.log('Where did all the dinosaurs go?');
     });
--- js/dinoTweet.js
+++ js/dinoTweet.js
@@ -1,17 +1,9 @@
-
+// 
 // exports.DinoTweet = function(newTweet) {
 //   this.newTweet = newTweet;
 // }
 //
-// exports.DinoTweet.create =
-// var xhr = new XMLHttpRequest();
+// exports.DinotTweet.prototype.create = function(response){
+//   newTweet.open('GET', http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=3&words=3),
 //
-// xhr.open('GET', 'http://dinoipsum.herokuapp.com/api/?format=html&words=10&paragraphs=3');
-// xhr.onload = function() {
-//   if (this.status === 200) {
-//     document.querySelector('#some-awesome-container').innerHTML = this.response;
-//   } else {
-//     console.log('Where did all the dinosaurs go?');
-//   }
-// };
-// xhr.send();
+// }
