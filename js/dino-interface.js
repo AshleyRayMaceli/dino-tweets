027b1117ea58ff4dc7ea4f96650fab790464874f
Ashley Maceli and Ryan Carlile <ryan@>
2016-05-26 10:47:56 -0700

    Add the conversion to back end with constructors methods

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,6 +1,28 @@
+// var apiKey = "YOUR-API-KEY-GOES-HERE";
+// var corpora = require('corpora-project');
+// var corporaProject = require('corpora-project');
+var DinoTweet = require('./../js/dinoTweet.js').DinoTweet;
+var getEmojis = require('./../js/dinoTweet.js').getEmojis;
+// var emojiAPI = DinoTweet.getEmojis();
+//
+// console.log(emojiAPI);
+
+
+
+// var displayFunction = function(dataReturnedFromAPI) {
+//   //a bunch of jquery in here, or parse your data from the api
+//   $('#emoji').html(response.seaEmoji[Math.floor((Math.random() * response.seaEmoji.length) + 1)]);
+// }
+
+
 $(document).ready(function() {
 
   $('.btn').click(function() {
+    // console.log(getEmojis());
+    getEmojis();
+    // var newEmoji = getEmojis();
+    //
+    // var newDinoTweet = new DinoTweet(newEmoji, ..);
 
     $.get('/verb.json').then(function(response) {
       $('#verb').html(response.verbs[Math.floor((Math.random() * response.verbs.length) + 1)].past);
--- js/dinoTweet.js
+++ js/dinoTweet.js
@@ -1,12 +1,25 @@
 //
-// exports.DinoTweet = function(newTweet) {
-//   this.newTweet = newTweet;
-// }
+exports.DinoTweet = function(emoji, verb, dino, job, fruit, corporation) {
+  this.emoji = emoji;
+  this.verb = verb;
+  this.dino = dino;
+  this.job = job;
+  this.fruit = fruit;
+  this.corporation = corporation;
+
+};
 //
-// exports.DinotTweet.prototype.create = function(myFunction){
-//   // newTweet.open('GET', http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=3&words=3),
+// exports.DinoTweet.prototype.create = function(myFunction){
 //
 //   $.get('/emoji.json').then(function(response) {
 //     myFunction(response);
 //   });
-// }
+
+exports.getEmojis = function() {
+  var newEmoji = $.get('/emoji.json').then(function(response) {
+    newEmoji = response.seaEmoji[Math.floor((Math.random() * response.seaEmoji.length) + 1)];
+    console.log(newEmoji);
+    return newEmoji;
+  });
+
+};
